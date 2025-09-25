558977359924c18e3ad9909959fa566233da7ee5
kyeong-bin <kyeongb0315@gmail.com>
2025-09-25 14:34:06 +0900

    feat: useContext를 활용하여 다크 모드 토글 구현

---
+++ week02/Gangbean_02/src/components/ThemeContent.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx';
+import { useTheme } from '../context/ThemeProvider';
+
+const ThemeContent = () => {
+    const { theme } = useTheme();
+    const isLightMode = theme === 'LIGHT';
+    return (
+        <div
+            className={clsx(
+                'p-4 h-dvh w-full',
+                isLightMode ? 'bg-white' : 'bg-gray-800'
+            )}
+        >
+            <h1
+                className={clsx(
+                    'text-wxl font-bold',
+                    isLightMode
+                        ? 'text-black'
+                        : 'text-white'
+                )}
+            >
+                Theme Content
+            </h1>
+            <p
+                className={clsx(
+                    'mt-2',
+                    isLightMode
+                        ? 'text-black'
+                        : 'text-white'
+                )}
+            >
+                Tailwind CSS works by scanning all of your
+                HTML files, JavaScript components, and any
+                other templates for class names, generating
+                the corresponding styles and then writing
+                them to a static CSS file.
+            </p>
+        </div>
+    );
+};
+
+export default ThemeContent;
---
+++ week02/Gangbean_02/src/context/ThemeProvider.tsx
@@ -0,0 +1,46 @@
+import {
+    createContext,
+    useContext,
+    useState,
+    type PropsWithChildren,
+} from 'react';
+
+export type TTHEME = 'LIGHT' | 'DARK';
+
+interface IThemeContextState {
+    theme: TTHEME;
+    toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<
+    IThemeContextState | undefined
+>(undefined);
+
+export const ThemeProvider = ({
+    children,
+}: PropsWithChildren) => {
+    const [theme, setTheme] = useState<TTHEME>('LIGHT');
+    const toggleTheme = () => {
+        setTheme(
+            (prev): TTHEME =>
+                prev === 'LIGHT' ? 'DARK' : 'LIGHT'
+        );
+    };
+    return (
+        <ThemeContext.Provider
+            value={{ theme, toggleTheme }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error(
+            'useTheme은 themeProvider에 감싸져야 해!'
+        );
+    }
+    return context;
+};
