868afd4cd7606fd6220ab619cbf0b497dca8d09f
kyeong-bin <kyeongb0315@gmail.com>
2025-09-25 09:16:25 +0900

    feat: TypeScript와 React를 활용하여 TodoList 구현

---
+++ week02/Gangbean_01/src/components/Todo.tsx
@@ -0,0 +1,39 @@
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import { useTodo } from '../context/TodoContext';
+
+const Todo = () => {
+    const {
+        todos,
+        doneTodos,
+        handleCompleteTodo,
+        handleDeleteTodo,
+    } = useTodo();
+
+    return (
+        <div className='todo-container'>
+            <h1 className='todo-container__header'>
+                GANGBEAN TODO
+            </h1>
+            <TodoForm />
+            <div className='render-container'>
+                <TodoList
+                    title='할 일'
+                    todos={todos}
+                    buttonLabel='완료'
+                    buttonColor='#28a745'
+                    onClick={handleCompleteTodo}
+                />
+                <TodoList
+                    title='완료'
+                    todos={doneTodos}
+                    buttonLabel='삭제'
+                    buttonColor='#dc3545'
+                    onClick={handleDeleteTodo}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
