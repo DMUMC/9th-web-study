a94591e116338aebad12cc67d23143da5aa66740
limtjdghks <epaktldk0203@gmail.com>
2025-09-30 20:02:06 +0900

    feat: 라우팅 설정

--- week03/Shawn_01/src/App.tsx
+++ week03/Shawn_01/src/App.tsx
@@ -1,13 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import MoviePage from './pages/MoviePage'
+import MainPage from './pages/MainPage'
+import ErrorPage from './pages/ErrorPage'
+import { Layout } from './components/layout/Layout'
 
 function App() {
-
-  return (
-    <>
-      <MoviePage />
-    </>
-  )
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	)
 }
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: '/',
+				element: <MainPage />,
+			},
+			{
+				path: 'movie/:category/:page',
+				element: <MoviePage />,
+			},
+		],
+	},
+])
 
 export default App
