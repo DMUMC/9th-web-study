74017ef819481634d3db2e347b07dbc30242fd25
kyeong-bin <kyeongb0315@gmail.com>
2025-10-02 17:36:17 +0900

    feat: 미션3 작업을 위한 프로젝트 세팅

---
+++ week03/Gangbean_03/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,8 @@
+import { useParams } from 'react-router-dom';
+
+const MovieDetailPage = () => {
+    const params = useParams();
+    return <>moviedetail</>;
+};
+
+export default MovieDetailPage;
