c9a23a92dd32014c998b39ff9f6e88aed3cff10e
limtjdghks <epaktldk0203@gmail.com>
2025-10-03 19:58:50 +0900

    feat: useCustomFetch 추가

--- week04/Shawn/src/pages/MoviePage.tsx
+++ week04/Shawn/src/pages/MoviePage.tsx
@@ -1,41 +1,19 @@
-import { useEffect, useState } from "react";
-import type { MovieResponseT, MovieT } from "../types/movie";
-import { api } from "../utils/AxiosInstance"
-import { MoivePoster } from "../components/MoivePoster"
 import { useParams, useNavigate } from 'react-router-dom'
+import { MoivePoster } from '../components/MoivePoster'
 import { Loading } from '../components/Loading'
 import { Pagination } from '../components/Pagination'
+import { useCustomFetch } from '../hooks/useCustomFetch'
+import type { MovieResponseT } from '../types/movie'
 
 const MoviePage = () => {
-	const [movieList, setMovieList] = useState<MovieT[] | null>([])
-	const [isLoading, setIsLoading] = useState<boolean>(false)
-	const [error, setError] = useState<string | null>(null)
 	const { category, page } = useParams()
 	const navigate = useNavigate()
 
 	const currentPage = parseInt(page || '1')
+	const url = `/movie/${category}?language=ko-KR&page=${page}`
 
-	// 영화 목록 조회 API 호출
-	useEffect(() => {
-		const getMovieList = async () => {
-			try {
-				setIsLoading(true)
-				setError(null) // 에러 상태 초기화
-				const res = await api.get<MovieResponseT>(`/movie/${category}?language=ko-KR&page=${page}`)
-				setMovieList(res.data.results)
-			} catch (error) {
-				console.error(error)
-				setError('영화 데이터를 불러오는데 실패했습니다.')
-			} finally {
-				setIsLoading(false)
-			}
-		}
-		getMovieList()
-	}, [category, page])
-
-	useEffect(() => {
-		console.log(movieList)
-	}, [movieList])
+	const { data: movieResponse, isLoading, error } = useCustomFetch<MovieResponseT>(url)
+	const movieList = movieResponse?.results || []
 
 	const handlePageChange = (page: number) => {
 		navigate(`/movie/${category}/${page}`)
