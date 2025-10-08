3674ede8a8618042c1084999f9aa21af3b176567
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-10-08 22:00:46 +0900

    feat : 4주차 미션 3

--- week04/Duck_02/src/App.tsx
+++ week04/Duck_02/src/App.tsx
@@ -5,6 +5,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 import LoginPage from "./pages/LoginPage";
 import HomeLayout from "./Layout/HomeLayout";
 import SignupPage from "./pages/SignupPage";
+import Mypage from "./pages/Mypage";
 // 1. 홈페이지
 // 2. 로그인 페이지
 // 3. 회원가입 페이지
@@ -25,8 +26,12 @@ function App() {
           element: <LoginPage />,
         },
         {
-          path: "signup",
+          path: "/signup",
           element: <SignupPage />,
+        },
+        {
+          path: "/mypage",
+          element: <Mypage />,
         },
       ],
     },
--- week04/Duck_02/src/pages/LoginPage.tsx
+++ week04/Duck_02/src/pages/LoginPage.tsx
@@ -1,7 +1,13 @@
-import useForm from "./hooks/userForm";
+import useForm from "../hooks/userForm";
+import { useLocalStorage } from "../hooks/UseLocalStorage";
+import { LocalStorageKey } from "../constants/key";
 import { type UserSigninInformation, validateSignin } from "../utils/validate";
-
+import { postLogin } from "../apis/auth";
+import InPutCompoent from "../components/InPutCompoent";
+import { useNavigate } from "react-router-dom";
 export default function LoginPage() {
+  const navigate = useNavigate();
+  const { setItem } = useLocalStorage(LocalStorageKey.accessToken);
   const { values, errors, touched, getInputProps } =
     useForm<UserSigninInformation>({
       initialValues: {
@@ -11,8 +17,16 @@ export default function LoginPage() {
       validate: validateSignin,
     });
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     console.log(values);
+    try {
+      const response = await postLogin(values);
+      setItem(response.data.accessToken);
+      console.log(response);
+      navigate("/mypage");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const isDisabled =
@@ -22,21 +36,20 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-full">
       <div className="flex flex-col gap-3">
-        <input
-          {...getInputProps("email")}
-          name="email"
-          className="border border-[#ccc] w-[300px] p-2 focus:border-[#333] rounded-md"
+        <InPutCompoent
           type="email"
           placeholder="Email"
+          {...getInputProps("email")}
         />
+
         {errors?.email && touched?.email && (
           <div className="text-red-500 text-sm">{errors?.email}</div>
         )}
-        <input
-          {...getInputProps("password")}
-          className="border border-[#ccc] w-[300px] p-2 focus:border-[#333] rounded-md"
+
+        <InPutCompoent
           type="password"
           placeholder="password"
+          {...getInputProps("password")}
         />
         {errors?.password && touched?.password && (
           <div className="text-red-500 text-sm">{errors?.password}</div>
