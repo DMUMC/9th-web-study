fbd19b5df028eccc351524abd3c177c003aef73d
sooh329 <glay@kakao.com>
2025-10-10 09:42:08 +0900

    feat: 비밀번호 가시성 버튼 추가

--- week04/Mori_Login/src/pages/SignupPage.tsx
+++ week04/Mori_Login/src/pages/SignupPage.tsx
@@ -6,6 +6,7 @@ import { SocialLogin } from "../components/SocialLogin";
 import { postSignup } from "../apis/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const schema = z.object({
   email: z.string().email({message: "올바른 이메일 형식이 아닙니다."}),
@@ -27,7 +28,7 @@ const schema = z.object({
     }),
   name: z
     .string()
-    .min(1, { message: "이름을 입력해주세요." })
+    .min(1, { message: "닉네임을 입력해주세요." })
 })
 .refine((data) => data.password === data.passwordCheck, {
   message: "비밀번호가 일치하지 않습니다. ",
@@ -39,7 +40,10 @@ type FormFields = z.infer<typeof schema>;
 export const SignupPage = () => {
 
   const [step, setStep] = useState(1)
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showPasswordCheck, setShowPasswordCheck] = useState(false)
 
   const {
     register, 
@@ -93,7 +97,7 @@ export const SignupPage = () => {
             <input 
               {...register('email')}
               type={'email'} 
-              className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-white placeholder-gray-400
+              className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-sm text-white placeholder-gray-400
                 ${errors?.email ? 'border-red-500': ''}`}
               placeholder={'이메일을 입력해주세요'}
             />
@@ -107,21 +111,37 @@ export const SignupPage = () => {
               <label className="text-sm text-gray-400">이메일</label>
               <p className="text-lg font-medium text-gray-200">{getValues('email')}</p>
             </div>
-            <input 
-              {...register('password')}
-              type={'password'} 
-              className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-white placeholder-gray-400
-                ${errors?.password ? 'border-red-500': ''}`}
-              placeholder={'비밀번호를 입력해주세요'}
-            />
+            <div className="relative w-full">
+              <input 
+                {...register('password')}
+                type={showPassword ? 'text' : 'password'}
+                className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-sm text-white placeholder-gray-400
+                  ${errors?.password ? 'border-red-500': ''}`}
+                placeholder={'비밀번호를 입력해주세요'}
+              />
+              <div 
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                onClick={() => setShowPassword(prev => !prev)}
+              >
+                {showPassword ? <FaEyeSlash color="gray" /> : <FaEye color="gray" />}
+              </div>
+            </div>
             {errors.password && <div className={`text-red-500 text-sm`}>{errors.password.message}</div>}
-            <input 
-              {...register('passwordCheck')}
-              type={'password'} 
-              className={`border border-white bg-[#202020] w-full p-3 mt-2 focus:outline-none focus:border-[#ff00b3] rounded-lg text-white placeholder-gray-400
-                ${errors?.passwordCheck ? 'border-red-500': ''}`}
-              placeholder={'비밀번호를 다시 한 번 입력해주세요'}
-            />
+            <div className="relative w-full">
+              <input 
+                {...register('passwordCheck')}
+                type={showPasswordCheck ? 'text' : 'password'}
+                className={`border border-white bg-[#202020] w-full p-3 mt-2 focus:outline-none focus:border-[#ff00b3] rounded-lg text-sm text-white placeholder-gray-400
+                  ${errors?.passwordCheck ? 'border-red-500': ''}`}
+                placeholder={'비밀번호를 다시 한 번 입력해주세요'}
+              />
+              <div 
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                onClick={() => setShowPasswordCheck(prev => !prev)}
+              >
+                {showPasswordCheck ? <FaEyeSlash color="gray" /> : <FaEye color="gray" />}
+              </div>
+            </div>
             {errors.passwordCheck && <div className={`text-red-500 text-sm`}>{errors.passwordCheck.message}</div>}
           </>
         )}
@@ -131,7 +151,7 @@ export const SignupPage = () => {
             <input 
               {...register('name')}
               type={'text'}
-              className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-white placeholder-gray-400
+              className={`border border-white bg-[#202020] w-full p-3 focus:outline-none focus:border-[#ff00b3] rounded-lg text-sm text-white placeholder-gray-400
                 ${errors?.name ? 'border-red-500': ''}`}
               placeholder={'닉네임을 입력해주세요'}
             />
@@ -158,7 +178,7 @@ export const SignupPage = () => {
             disabled={isSubmitting || !!errors.name || getValues('name') === ''} 
             className='w-full bg-[#ff00b3] text-white py-3 mt-2 rounded-md font-medium 
             hover:bg-[#b3007d] transition-colors cursor-pointer disabled:bg-[#202020] disabled:text-gray-500'>
-              회원가입
+              회원가입 완료
           </button>
         )}
       </div>
