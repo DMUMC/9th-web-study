e11503a73fbf5dcf1b9e9f8086c716c8cd0d2cad
GitHub_Seongmin0218 <GitHub_yyggy365@gmail.com>
2025-10-10 11:01:41 +0900

    4week workbook 01

---
+++ week04/Balvenie_01/src/types/movie.ts
@@ -0,0 +1,33 @@
+export interface MovieT {
+    id: number
+    title: string
+    overview: string
+    poster_path: string
+    release_date: string
+    vote_average: number
+    vote_count: number
+}
+
+export interface MovieResponseT {
+    results: MovieT[]
+    page: number
+    total_pages: number
+    total_results: number
+}
+
+export interface MovieDetailT {
+	id: number
+	title: string
+	overview: string
+	poster_path: string
+	backdrop_path: string
+	release_date: string
+	vote_average: number
+	vote_count: number
+	runtime: number
+	genres: { id: number; name: string }[]
+	tagline: string
+	status: string
+	budget: number
+	revenue: number
+}
