d5716c561bc469531bd333cae9fb7b370dbf3c27
GitHub_Seongmin0218 <GitHub_yyggy365@gmail.com>
2025-10-11 22:53:26 +0900

    토큰관리방식 개선

--- week04/Balvenie_03/src/api/auth.ts
+++ week04/Balvenie_03/src/api/auth.ts
@@ -1,26 +1,24 @@
 import { LOCAL_STORAGE_KEY } from "../constant/key";
-import type { RequestLoginDto, RequestSignupDto, ResponseLoginDto, ResponseMyInfoDto, ResponseSignupDto } from "../types/auth";
+import type {
+  RequestLoginDto,
+  RequestSignupDto,
+  ResponseLoginDto,
+  ResponseMyInfoDto,
+  ResponseSignupDto
+} from "../types/auth";
 import { apiAuth } from "../utils/AxiosInstance";
 
-export const postSignup = async (body: RequestSignupDto):Promise<ResponseSignupDto> => {
-    const {data} = await apiAuth.post('/signup', body)
+export const postSignup = async (body: RequestSignupDto): Promise<ResponseSignupDto> => {
+  const { data } = await apiAuth.post('/signup', body);
+  return data;
+};
 
-    return data
-}
+export const postLogin = async (body: RequestLoginDto): Promise<ResponseLoginDto> => {
+  const { data } = await apiAuth.post('/signin', body);
+  return data;
+};
 
-export const postLogin = async (body: RequestLoginDto):Promise<ResponseLoginDto> => {
-    const {data} = await apiAuth.post('/signin', body)
-
-    return data
-}
-
-export const getMyInfo = async ():Promise<ResponseMyInfoDto> => {
-    const {data} = await apiAuth.get('/users/me', {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN)}`
-            }
-        }
-    )
-
-    return data
-}
+export const getMyInfo = async (): Promise<ResponseMyInfoDto> => {
+  const { data } = await apiAuth.get('/users/me');
+  return data;
+};
