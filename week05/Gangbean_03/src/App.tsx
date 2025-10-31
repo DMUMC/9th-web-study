ba85e7525dcff440ae8e4b391a251677a93dc596
kyeong-bin <kyeongb0315@gmail.com>
2025-10-31 16:53:03 +0900

    feat: 구글 소셜 로그인 기능 구현

--- week05/Gangbean_03/src/App.tsx
+++ week05/Gangbean_03/src/App.tsx
@@ -12,6 +12,7 @@ import SignupPage from './pages/SignupPage';
 import MyPage from './pages/MyPage';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedLayout from './layouts/ProtectedLayout';
+import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage';
 
 const publicRoutes: RouteObject[] = [
     {
@@ -30,6 +31,10 @@ const publicRoutes: RouteObject[] = [
             {
                 path: 'signup',
                 element: <SignupPage />,
+            },
+            {
+                path: 'v1/auth/google/callback',
+                element: <GoogleLoginRedirectPage />,
             },
         ],
     },
