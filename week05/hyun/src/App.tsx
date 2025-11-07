87d49a9b3a7664304f6e1706419f6aaa1ff5a8ca
sisihhy <sihyun7369@gmail.com>
2025-11-07 21:46:25 +0900

    미션2

---
+++ week05/hyun/src/App.tsx
@@ -0,0 +1,52 @@
+import {
+    RouterProvider,
+    createBrowserRouter,
+    type RouteObject,
+} from 'react-router-dom';
+import './App.css';
+import Homepage from './pages/Homepage';
+import NotFound from './pages/NotFound';
+import LoginPage from './pages/LoginPage';
+import HomeLayout from './layouts/HomeLayout';
+import SignupPage from './pages/SignupPage';
+import MyPage from './pages/MyPage';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedLayout from './layouts/ProtectedLayout';
+
+// publicRoutes: 인증이 필요하지 않은 라우트
+const publicRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <HomeLayout />,
+        errorElement: <NotFound />,
+        children: [
+            { index: true, element: <Homepage /> },
+            { path: 'login', element: <LoginPage /> },
+            { path: 'signup', element: <SignupPage /> },
+        ],
+    },
+];
+// protectedRoutes: 인증이 필요한 라우트
+const protectedRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <ProtectedLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: 'my',
+                element: <MyPage />,
+            },
+        ],
+    },
+];
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+function App() {
+    return (
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    );
+}
+
+export default App;
