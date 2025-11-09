679c3ddf25242938f81f8323b7fe107b9bc35216
limtjdghks <epaktldk0203@gmail.com>
2025-11-10 03:03:14 +0900

    feat: 로직 개선

--- week06/Shawn/src/components/LpCard.tsx
+++ week06/Shawn/src/components/LpCard.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router"
 import type { Likes } from "../types/lp"
+import { formatRelativeTime } from "../utils/formatRelativeTime"
 
 interface LpCardProps {
     id: number
@@ -24,7 +25,7 @@ export const LpCard = ({id, title, thumbnail, createdAt, likes}: LpCardProps) =>
                     <div className='mt-auto flex flex-col gap-2 h-full justify-end'>
                         <p className='text-lg font-bold text-white group-hover:opacity-100 transition-opacity duration-300 line-clamp-3'>{title}</p>
                         <div className='flex items-center gap-2'>
-                            <p className="text-sm text-neutral-400 group-hover:opacity-100 transition-opacity duration-300">{new Date(createdAt).toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric'})}</p>
+                            <p className="text-sm text-neutral-400 group-hover:opacity-100 transition-opacity duration-300">{formatRelativeTime(createdAt)}</p>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
                             <p className="text-sm text-neutral-400 group-hover:opacity-100 transition-opacity duration-300">{likes.length}</p>
                         </div>
