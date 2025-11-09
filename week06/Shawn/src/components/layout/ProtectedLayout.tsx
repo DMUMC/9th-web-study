be5323457868a50233aadf1f2cccf2434dc14577
limtjdghks <epaktldk0203@gmail.com>
2025-11-10 02:59:13 +0900

    feat: 보호라우팅 개선

--- week06/Shawn/src/components/layout/ProtectedLayout.tsx
+++ week06/Shawn/src/components/layout/ProtectedLayout.tsx
@@ -1,19 +1,29 @@
-import { Navigate } from "react-router"
-import { useAuth } from "../../context/AuthContext"
-import { Layout } from "./Layout"
+import { Navigate, useLocation } from 'react-router'
+import { useEffect, useRef } from 'react'
+import { useAuth } from '../../context/AuthContext'
+import { Layout } from './Layout'
 
 const ProtectedLayout = () => {
-    const {accessToken} = useAuth()
+	const { accessToken } = useAuth()
+	const location = useLocation()
+	const hasAlertedRef = useRef(false)
 
-    if(!accessToken) {
-        return <Navigate to="/login" replace />
-    }
+	useEffect(() => {
+		if (!accessToken && !location.state?.from && !hasAlertedRef.current) {
+			alert('로그인이 필요한 서비스입니다.')
+			hasAlertedRef.current = true
+		}
+	}, [accessToken, location.state])
 
-    return (
-        <>
-            <Layout />
-        </>
-    )
+	if (!accessToken) {
+		return <Navigate to='/login' replace state={{ from: location }} />
+	}
+
+	return (
+		<>
+			<Layout />
+		</>
+	)
 }
 
 export default ProtectedLayout
--- week06/Shawn/src/context/AuthContext.tsx
+++ week06/Shawn/src/context/AuthContext.tsx
@@ -5,61 +5,58 @@ import  type { RequestLoginDto } from "../types/auth"
 import { createContext, useContext, useState, type PropsWithChildren } from "react"
 
 export interface AuthContextType {
-    accessToken: string | null
-    refreshToken: string | null
-    login: (signinData: RequestLoginDto) => Promise<void>
-    logout: () => void
+	accessToken: string | null
+	refreshToken: string | null
+	login: (signinData: RequestLoginDto) => Promise<boolean>
+	logout: () => void
 }
 
 export const AuthContext = createContext<AuthContextType>({
-    accessToken: null,
-    refreshToken: null,
-    login: async () => {},
-    logout: () => {}
+	accessToken: null,
+	refreshToken: null,
+	login: async () => false,
+	logout: () => {},
 })
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-   const {setItem: setAccessTokenStorage, getItem: getAccessTokenStorage, removeItem: removeAccessTokenStorage} = useLocalStorage(LOCAL_STORAGE_KEY.ACCESS_TOKEN)
-   const {setItem: setRefreshTokenStorage, getItem: getRefreshTokenStorage, removeItem: removeRefreshTokenStorage} = useLocalStorage(LOCAL_STORAGE_KEY.REFRESH_TOKEN)
+	const { setItem: setAccessTokenStorage, getItem: getAccessTokenStorage, removeItem: removeAccessTokenStorage } = useLocalStorage(LOCAL_STORAGE_KEY.ACCESS_TOKEN)
+	const { setItem: setRefreshTokenStorage, getItem: getRefreshTokenStorage, removeItem: removeRefreshTokenStorage } = useLocalStorage(LOCAL_STORAGE_KEY.REFRESH_TOKEN)
 
-   const [accessToken, setAccessToken] = useState<string | null>(getAccessTokenStorage())
-   const [refreshToken, setRefreshToken] = useState<string | null>(getRefreshTokenStorage())
+	const [accessToken, setAccessToken] = useState<string | null>(getAccessTokenStorage())
+	const [refreshToken, setRefreshToken] = useState<string | null>(getRefreshTokenStorage())
 
-   const login = async (signinData: RequestLoginDto) => {
-        try {
-            const {data} = await postLogin(signinData)
-            setAccessToken(data.accessToken)
-            setRefreshToken(data.refreshToken)
-            setAccessTokenStorage(data.accessToken)
-            setRefreshTokenStorage(data.refreshToken)
-            alert('로그인에 성공했습니다.')
-            window.location.href = '/mypage'
-        } catch (error) {
-            console.error(error)
-            alert('로그인에 실패했습니다.')
-        }
-   }
+	const login = async (signinData: RequestLoginDto) => {
+		try {
+			const { data } = await postLogin(signinData)
+			setAccessToken(data.accessToken)
+			setRefreshToken(data.refreshToken)
+			setAccessTokenStorage(data.accessToken)
+			setRefreshTokenStorage(data.refreshToken)
+			alert('로그인에 성공했습니다.')
+			return true
+		} catch (error) {
+			console.error(error)
+			alert('로그인에 실패했습니다.')
+			return false
+		}
+	}
 
-   const logout = async () => {
-    try {
-        await postLogout()
-        setAccessToken(null)
-        setRefreshToken(null)
-        removeAccessTokenStorage()
-        removeRefreshTokenStorage()
-        alert('로그아웃에 성공했습니다.')
-        window.location.href = '/'
-    } catch (error) {
-        console.error(error)
-        alert('로그아웃에 실패했습니다.')
-    }
-   }
+	const logout = async () => {
+		try {
+			await postLogout()
+			setAccessToken(null)
+			setRefreshToken(null)
+			removeAccessTokenStorage()
+			removeRefreshTokenStorage()
+			alert('로그아웃에 성공했습니다.')
+			window.location.href = '/'
+		} catch (error) {
+			console.error(error)
+			alert('로그아웃에 실패했습니다.')
+		}
+	}
 
-   return (
-    <AuthContext.Provider value={{accessToken, refreshToken, login, logout}}>
-        {children}
-    </AuthContext.Provider>
-   )
+	return <AuthContext.Provider value={{ accessToken, refreshToken, login, logout }}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => {
