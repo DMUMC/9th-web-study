b9a53bb510b4f1d882c65c9616d3adb53f7828ff
sooh329 <glay@kakao.com>
2025-11-14 17:50:01 +0900

    feat: 프로젝트 복사

---
+++ week07/Mori/src/components/AuthHeader.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+interface AuthHeaderProps {
+  title: string;
+}
+
+export const AuthHeader = ({ title }: AuthHeaderProps) => {
+  return (
+    <div className='relative flex justify-center items-center mb-4 text-white'>
+      <Link to="/" className="absolute left-0">
+        <div className='text-2xl cursor-pointer'>&lt;</div>
+      </Link>
+      <h2 className='text-2xl font-bold'>{title}</h2>
+    </div>
+  );
+};
