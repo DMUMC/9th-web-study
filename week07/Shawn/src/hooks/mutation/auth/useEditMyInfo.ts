2d240f4014efee8177fb6570b2b0daf45f6c5174
limtjdghks <epaktldk0203@gmail.com>
2025-11-17 21:46:43 +0900

    feat: 마이페이지 낙관적 업데이트 적용

--- week07/Shawn/src/hooks/mutation/auth/useEditMyInfo.ts
+++ week07/Shawn/src/hooks/mutation/auth/useEditMyInfo.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { editMyInfo } from "../../../apis/auth"
 import { QUERY_KEY } from "../../../constant/key"
+import type { ResponseMyInfoDto } from "../../../types/auth"
 import type { RequestEditMyInfoDto } from "../../../types/auth"
 
 export const useEditMyInfo = () => {
@@ -11,5 +12,29 @@ export const useEditMyInfo = () => {
         onSuccess: () => {
             qc.invalidateQueries({ queryKey: [QUERY_KEY.myinfo] })
         },
+        onMutate: async (body: RequestEditMyInfoDto) => {
+            await qc.cancelQueries({ queryKey: [QUERY_KEY.myinfo] })
+
+            const previousMyInfo = qc.getQueryData<ResponseMyInfoDto>([QUERY_KEY.myinfo])
+
+            const newMyInfo = {...previousMyInfo} as ResponseMyInfoDto
+
+            newMyInfo.data.name = body.name
+            newMyInfo.data.bio = body.bio
+            newMyInfo.data.avatar = body.avatar ?? null
+
+            qc.setQueryData([QUERY_KEY.myinfo], newMyInfo)
+
+            return { previousMyInfo, newMyInfo }
+        },
+
+        onError: (error, _, context) => {
+            console.error(error)
+            qc.setQueryData([QUERY_KEY.myinfo], context?.previousMyInfo)
+        },
+
+        onSettled: async () => {
+            await qc.invalidateQueries({ queryKey: [QUERY_KEY.myinfo] })
+        }
     })
 }
