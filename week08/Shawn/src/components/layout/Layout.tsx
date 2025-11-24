2033229c5e435e888d102370db4c64d5c89cdddf
limtjdghks <epaktldk0203@gmail.com>
2025-11-24 17:00:42 +0900

    feat: 사이드바 개선선

--- week08/Shawn/src/components/layout/Layout.tsx
+++ week08/Shawn/src/components/layout/Layout.tsx
@@ -8,12 +8,13 @@ import useLpModal from '../../store/useLpModal'
 import { useLeaveModal } from '../../store/useLeaveModal'
 import { LeaveModal } from './LeaveModal'
 import { useThrottleCallback } from '../../hooks/useThrottle'
+import useSidebar from '../../hooks/useSidebar'
 
 export const Layout = () => {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 	const [isDesktop, setIsDesktop] = useState(false)
 	const { isOpen } = useLpModal()
 	const { isLeaveModalOpen } = useLeaveModal()
+	const { isOpen: sidebarOpen, close, toggle } = useSidebar()
 
 	// resize 이벤트에 throttle 적용 (300ms마다 한 번만 실행)
 	const handleResize = useThrottleCallback(
@@ -37,25 +38,17 @@ export const Layout = () => {
 
 	useEffect(() => {
 		if (isDesktop) {
-			setIsSidebarOpen(false)
+			close()
 		}
-	}, [isDesktop])
-
-	const handleToggleSidebar = () => {
-		setIsSidebarOpen((prev) => !prev)
-	}
-
-	const handleCloseSidebar = () => {
-		setIsSidebarOpen(false)
-	}
+	}, [isDesktop, close])
 
 	return (
 		<div className='bg-neutral-900 text-neutral-200 min-h-screen min-w-screen flex flex-col'>
 			{isOpen && <LpModal />}
 			{isLeaveModalOpen && <LeaveModal />}
-			<Navbar onMenuClick={handleToggleSidebar} />
+			<Navbar onMenuClick={toggle} />
 			<div className='flex flex-1'>
-				<Sidebar isOpen={isSidebarOpen || isDesktop} onClose={handleCloseSidebar} />
+				<Sidebar isOpen={isDesktop || sidebarOpen} onClose={close} />
 				<FloatingButton />
 				<div className='flex-1 flex justify-center'>
 					<Outlet />
