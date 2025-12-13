8bf91363c311a1ce4955277272b9ab2d50240d37
GitHub_Seongmin0218 <GitHub_yyggy365@gmail.com>
2025-12-13 18:10:37 +0900

    Balvenie week10 useMemo

---
+++ week10/Balvenie_useMemo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import UseMemoPage from './page/UseMemoPage';
+
+function App() {
+    return (
+        <main className='flex flex-col items-center justify-center h-dvh'>
+            <UseMemoPage />
+        </main>
+    );
+}
+
+export default App;
