36a2177a983570930aa0d8e6df2c9ce4ec6f93d9
SeongHwan <epaktldk0203@gmail.com>
2025-12-21 04:25:30 +0900

    [week04] 숀 4주차 미션_2,3 (#50)
    * chore: 프로젝트 생성
    
    * chore: TailwindCSS 라이브러리 추가
    
    * chore: 라이브러리 추가
    
    * feat: 로그인 페이지 구현
    
    * feat: useForm 커스텀훅 추가
    
    * chore: 라이브러리 추가
    
    * chore: .env 추가
    
    * chore: 아이콘 추가
    
    * feat: 로그인 / 회원가입 구현
    
    * feat: 로그인 로직 react-hook-form 사용하게끔 수정

---
+++ week04/Shawn_02/src/pages/MyPage.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import { getMyInfo } from "../apis/auth"
+
+const MyPage = () => {
+
+    useEffect(() => {
+        const getData = async () => {
+            await getMyInfo().then((res) => {
+                console.log(res)
+            })
+        }
+
+        getData()
+    },[])
+    return (
+        <div>
+            <h1>My Page</h1>
+        </div>
+    )
+}
+
+export default MyPage
