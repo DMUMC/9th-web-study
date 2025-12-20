3069d3d3b77ab4668ced2b4cd1443d23329b50a5
chiyyy <hyein031114@gmail.com>
2025-12-20 20:02:53 +0900

    feat: useMemo로 최적화

---
+++ week10/Hyeni_useMemo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import UseMemoPage from './pages/UseMemoPage'
+
+function App() {
+  return (
+    <main className='flex flex-col items-left justify-left h-dvh'>
+      <UseMemoPage />
+    </main>
+  )
+}
+
+export default App
