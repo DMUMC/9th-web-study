5f874f11bd71185561532d039d6b0510f993d89e
Seongmin0218 <169173727+Seongmin0218@users.noreply.github.com>
2025-12-21 04:29:55 +0900

    [week04] 발베니 4주차 미션_3 (#65)
    * 4week workbook 02
    
    * 4week workbook 03
    
    * 토큰관리방식 개선
    
    ---------
    
    Co-authored-by: GitHub_Seongmin0218 <GitHub_yyggy365@gmail.com>

---
+++ week04/Balvenie_03/src/components/layouts/Layouts.tsx
@@ -0,0 +1,21 @@
+import { NavLink, Outlet, useNavigate } from "react-router"
+
+export const Layout = () => {
+    const navigate = useNavigate()
+
+    return (
+        <div className='bg-neutral-900 text-neutral-200 min-h-screen min-w-screen flex flex-col'>
+            <div className='flex justify-between items-center p-4 bg-neutral-800'>
+                <p className='font-bold text-2xl text-teal-500 hover:cursor-pointer' onClick={() => navigate('/')}>돌려돌려  LP판</p>
+                <div className='flex gap-4'>
+                    <NavLink className='rounded-2xl bg-neutral-900 px-4 py-2 font-bold' to='/login'>로그인</NavLink>
+                    <NavLink className='rounded-2xl bg-teal-500 px-4 py-2 font-bold' to='/signup'>회원가입</NavLink>
+                </div>
+            </div>
+
+            <div className='flex-1 flex justify-center items-center'>
+                <Outlet />
+            </div>
+        </div>
+    )
+}
